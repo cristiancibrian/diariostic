5e9a53da9b45ff805a920d37b9fe7cd2fe3350ec
cristiancibrian <cristian.cibrian@uabc.edu.mx>
2024-11-19 13:18:18 +0100

    Portada actualizada

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada",
+"titulo" => "Portada Actualizada",
 "autor" => "mtorres",
 "resumen" => "El resumen maravilloso",
 ];
